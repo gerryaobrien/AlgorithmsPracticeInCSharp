bfab9e0536aa1301c9bde21a807ddd14b417c0d0
gerryaobrien <gerry.obrien@outlook.com>
2017-04-21 12:39:50 -0700

    Added greatest common denom function

--- AlgorithmPractice/Program.cs
+++ AlgorithmPractice/Program.cs
@@ -10,22 +10,43 @@ namespace AlgorithmPractice
     {
         static void Main(string[] args)
         {
-            int[] testArray = { 1, 2, 5, 9, 8, 5 };
+            int[] testArray = { 1, 2, 5, 9, 8, 5 };     // used for some of the algorithms below
+
             //int result = LargestInArray(testArray);
             //bool result = ContainsDuplicates(testArray);
             //Console.WriteLine("Result is: " + result);
 
-            Console.WriteLine("Original Array: ");
-            foreach(int item in testArray)
-            {
-                Console.Write(item);
-            }
-            Console.WriteLine();
+            #region Call RandomizeArray
 
-            RandomizeArray(testArray);
+            //Console.WriteLine("Original Array: ");
+            //foreach(int item in testArray)
+            //{
+            //    Console.Write(item);
+            //}
+            //Console.WriteLine();
+
+            //RandomizeArray(testArray);
+            #endregion
+
+            // Call GreatestCommonDenominator
+            // get  two values from the command line first
+            Console.WriteLine("Enter a single integer value: ");
+            int first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your second integer value: ");
+            int second = int.Parse(Console.ReadLine());
+
+            int gcd = GreatestCommonDenominator(first, second);
+            Console.WriteLine("The greatest common denominator of {0}, and {1} is: {2} ", first, second, gcd);
         }
 
+
+        #region Methods that implement various numeric algorithms
+
+
         // Largest in array - change the data type for non-integer arrays
+        // this algorithm will check all the values in a numeric array
+        // and will return the largest value found
         static int LargestInArray(int[] vals)
         {
             int largest = vals[0];
@@ -41,6 +62,10 @@ namespace AlgorithmPractice
             return largest;
         }
 
+        // ContainsDuplicates will check an array to see if there are duplicate values
+        // currently set to us an integer array but you can change the data type of the array
+        // to check for others
+        // If you use non-numerics such as char or string, you may need to rewrite the comparison logic
         static bool ContainsDuplicates(int[] vals)
         {
             bool result = false;
@@ -62,6 +87,8 @@ namespace AlgorithmPractice
             return result;
         }
 
+        // RandomizeArray takes an integer array and will randomize the values in the array
+        // This method will also output the randomized array to a console window using a foreach construct
         static void RandomizeArray(int[] vals)
         {
             int max = vals.Length;
@@ -84,6 +111,22 @@ namespace AlgorithmPractice
             Console.WriteLine();
         }
 
+        // GreatestCommonDivisor - The greatest common divisor (GCD) of two integers 
+        // is the largest integer that evenly divides both of the numbers.
+
+        static int GreatestCommonDenominator(int a, int b)
+        {
+            
+            while(b != 0)       // continue the loop until we reduce b to 0
+            {
+                int remainder = a % b;  // modulus operation to get remainder of a divided by b
+                a = b;                  // swap a and b so we can continue the loop
+                b = remainder;          // make b = to the remainder as that is what was left over from the modulus operation
+            }
+
+            return a;   // Once b has reached 0, that greatest common divisor is no present in a
+        }
+        #endregion
     }
 
 }
